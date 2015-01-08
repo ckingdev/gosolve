7ff1f77e806ea583fe29b0a8ed556cb4ac68ca7e
Cameron Palone <mcameronpalone@gmail.com>
2015-01-07 18:07:23 -0700

    Refactored PruningTable into a class and added a lookup function.

--- PruningTable.go
+++ PruningTable.go
@@ -4,31 +4,40 @@ import (
 	"fmt"
 )
 
-type PruningTable map[Puzzle]int8
+// type PruningTable map[Puzzle]int8
+type PruningTable struct {
+	max_depth int8
+	table     map[Puzzle]int8
+}
 
-func dls(pt *PruningTable, p *Puzzle, depth int, max_depth int, move_set []Move) {
+func PTLookup(pt *PruningTable, p *Puzzle) int8 {
+	val, ok := pt.table[*p]
+	if ok {
+		return val
+	} else {
+		return pt.max_depth + 1
+	}
+}
+
+func dls(pt *PruningTable, p *Puzzle, depth int8, max_depth int8, move_set []Move) {
 	if depth > 0 {
-		// _, ok := pt[p]
-		// if ok {
-		// 	return
-		// }
 		new_ps := p.ApplyMoves(move_set)
 		for _, new_p := range new_ps {
 			dls(pt, &new_p, depth-1, max_depth, move_set)
 		}
 	} else {
-		_, ok := (*pt)[*p]
+		_, ok := pt.table[*p]
 		if !ok {
-			(*pt)[*p] = int8(max_depth)
+			pt.table[*p] = int8(max_depth)
 		}
 	}
 }
 
-func GetPruningTable(p Puzzle, max_depth int, move_set []Move) PruningTable {
-	pt := make(PruningTable)
-	for d := 0; d <= max_depth; d++ {
+func GetPruningTable(p Puzzle, max_depth int8, move_set []Move) PruningTable {
+	pt := PruningTable{max_depth, make(map[Puzzle]int8)}
+	for d := int8(0); d <= max_depth; d++ {
 		dls(&pt, &p, d, d, move_set)
-		fmt.Printf("Depth: %v: %v\n", d, len(pt))
+		fmt.Printf("Depth: %v: %v\n", d, len(pt.table))
 	}
 	return pt
 }
